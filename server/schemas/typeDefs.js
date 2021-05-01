25814844d402377ec6f2366170cfadbcad8ed8a5
MalekBenJemia <malekbenjemia@gmail.com>
2021-05-01 00:17:43 -0400

    added schema and changed server.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,37 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: ID
+    authors: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    books(username: String): [Book]
+    book(_id: ID!): Book
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addBook(bookText: String!): Book
+  }
+`;
+
+module.exports = typeDefs;
